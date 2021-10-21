e562858fc6d693a484e09a86b020befa29522bc3
SergeiPozharkov <pozharkov.sergei@mail.ru>
2021-10-22 00:44:08 +0300

    added ny timer

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const newYear = "1 Jan 2022";
+
+function countDown() {
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const allSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(allSeconds / 3600 / 24);
+
+    const hours = Math.floor(allSeconds / 3600) % 24;
+
+    const minutes = Math.floor(allSeconds / 60) % 60;
+
+    const seconds = Math.floor(allSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+countDown();
+
+setInterval(countDown, 1000);
